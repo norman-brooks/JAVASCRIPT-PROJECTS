5cd9e8545f4496acf729d4f720d3edf8d5dbcb09
norman-brooks <nbprogramming@outlook.com>
2024-11-30 15:13:59 -0800

    Commit

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,12 @@
-   alert("Hello, World!");
+//Pop-up window
+window.alert("JS Sub Assignment");
+
+//Create Two Variables
+var Sent1="This is the beginning of the string "
+var Sent2="and this is the end of the string. "
+
+//Concatenate
+document.write(Sent1 + Sent2);
+
+//Expression
+document.write( 3 * 7 + 5);
