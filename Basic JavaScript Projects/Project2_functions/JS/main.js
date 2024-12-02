bf3c44eb044ff0a273e2a55fc40947889d01b046
norman-brooks <nbprogramming@outlook.com>
2024-12-02 14:14:22 -0800

    Commit for Step 70

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,12 +1,17 @@
+//Button to display the date and time
 function displayDate() {
     var str = "This is the button text";
     document.getElementById("Button_Text").innerHTML = Date();
 }
+//Used to change the color of the font on "x"
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('x').style.color = 'green';
+});
 
-function myFunction(a, b) {
-    //function returns the product of a and b
-    return a * b;
+//Operations function to join sentences when clicked
+function myFunction(){
+    var sentence="I have been a student of The Tech Academy";
+    sentence += " since August of 2024";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
 
-let result = myFunction(4, 3);
-document.getElementById('x').innerHTML=result;
