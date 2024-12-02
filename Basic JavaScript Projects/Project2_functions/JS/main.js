6a42b0d52f43287acfa14a84fdb919049ceda036
norman-brooks <nbprogramming@outlook.com>
2024-12-02 08:52:17 -0800

    commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+function displayDate() {
+    var str="This is the button text";
+    document.getElementById("Button_Text").innerHTML = Date();
+}
+function changeFontColor (green, color) {
+    var str="green";
+    document.getElemenytById("green").style.color = "#006400";
+}
