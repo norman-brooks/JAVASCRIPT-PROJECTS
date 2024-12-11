a3dc14ac25130be60ebf7423eead11cb40853ad1
Norman-Brooks <nbprogramming@outlook.com>
2024-12-11 06:07:53 -0800

    Commit

---
+++ Basic JavaScript Projects/Project7_scope-time_function/JS/main.js
@@ -0,0 +1,18 @@
+//Local and Global Variable Assignment
+
+var x = 20;
+function Add(){
+    document.write(25 + x);
+    console.log( 25 + x);
+}
+
+function Subtract(){
+    var y = 15;
+    document.write (25-y);
+    console.log (25 -y);
+}
+var z = 5;
+function Division(){
+    document.write(25 / z);
+    console.log (25 / z);
+}
